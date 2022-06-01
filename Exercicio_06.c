c8ed275b26f376fd7da919bd78f72f66d1735123
Matheus <54650216+Matheus-gs@users.noreply.github.com>
2022-06-01 20:02:01 -0300

    :tada: Ordenação decrescente adicionada

--- Exercicio_06.c
+++ Exercicio_06.c
@@ -1,4 +1,3 @@
-
 // Arquivo Exercicio_06.c
 // Autor: Matheus Gomes Santos - RGM:26136805
 // Turma: 3/N1
@@ -11,8 +10,8 @@ int busca_bin(int vet[], int n, int elem);
 int main(void)
 {
 
-	// int matriculas[7] = {100, 80, 75, 60, 52, 25, 10};
-	int matriculas[7] = {10, 25, 52, 60, 75, 80, 100};
+	int matriculas[7] = {100, 80, 75, 60, 52, 25, 10};
+
 	int n, matricula, resultado;
 
 	n = 7;
@@ -23,25 +22,33 @@ int main(void)
 
 	if (matriculas[resultado] == matricula)
 	{
-		printf("voce foi selecionado, sua matricula: %d", matricula);
+		printf("\n voce foi selecionado, sua matricula: %d \n", matricula);
+		printf(" \n o resultado retornado foi: %d \n", resultado);
 	}
 	else
 	{
-		printf("voce nao foi selecionado, sua matricula %d nao consta no sistema", matricula);
+		printf("\n voce nao foi selecionado, sua matricula %d nao consta no sistema \n", matricula);
+		printf(" \n o resultado retornado foi: %d \n", resultado);
 	}
 
 	matricula = 200;
 	resultado = busca_bin(matriculas, n, matricula);
+	
+	printf("\n ===== Segunda execucao ===== \n");
+	
 
-	printf(" \n voce buscou a matricula %d \n", matricula);
+	printf("\n voce buscou a matricula: %d \n", matricula);
+	
 
 	if (resultado == matricula)
 	{
-		printf("voce foi selecionado, sua matricula: %d", matricula);
+		printf("\n voce foi selecionado, sua matricula: %d \n", matricula);
+		printf("\n o resultado retornado foi: %d \n", resultado);
 	}
 	else
 	{
-		printf("voce nao foi selecionado, sua matricula %d nao consta no sistema", matricula);
+		printf("\n voce nao foi selecionado, sua matricula %d nao consta no sistema \n", matricula);
+		printf(" \n o resultado retornado foi: %d \n", resultado);
 	}
 
 	return 0;
@@ -54,8 +61,6 @@ int busca_bin(int vet[], int n, int elem)
 	ini = 0;
 	fim = n - 1;
 	
-	
-
 	while (ini <= fim)
 	{
 		meio = (ini + fim) / 2;
@@ -63,7 +68,7 @@ int busca_bin(int vet[], int n, int elem)
 		if (elem == vet[meio])
 			return meio;
 
-		else if (elem < vet[meio])
+		else if (elem > vet[meio])
 			fim = meio - 1;
 
 		else
